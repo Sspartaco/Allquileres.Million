9c702ced8c825501dd0525ac3c760ea6d0a45a53
Jonathan <jonathan.rojas@doctus.com.co>
2021-12-27 12:22:49 -0500

    Adición de primera migración.

---
+++ Alquileres.Domain/Entities/PropertyImage.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alquileres.Domain
+{
+    /// <summary>
+    /// Imagenes de la propiedad
+    /// </summary>
+    public class PropertyImage
+    {
+        /// <summary>
+        /// Id propiedad imagen
+        /// </summary>
+        [Key]
+        public string IdPropertyImage { get; set; }
+
+        /// <summary>
+        /// Id propiedad (FK)
+        /// </summary>
+        public string IdProperty { get; set; }
+
+        /// <summary>
+        /// Archivo
+        /// </summary>
+        public string File { get; set; }
+
+        /// <summary>
+        /// ¿Habilitado?
+        /// </summary>
+        public bool Enable { get; set; }
+    }
+}
---
+++ Alquileres.Infrastructuree/Migrations/20211227171545_Creacion-Base-Datos.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Alquileres.Infrastructuree.Migrations
+{
+    public partial class CreacionBaseDatos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Property",
+                columns: table => new
+                {
+                    IdProperty = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<string>(type: "TEXT", nullable: true),
+                    CodeInternal = table.Column<string>(type: "TEXT", nullable: true),
+                    Year = table.Column<int>(type: "INTEGER", nullable: false),
+                    IdOwner = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Property", x => x.IdProperty);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyImage",
+                columns: table => new
+                {
+                    IdPropertyImage = table.Column<string>(type: "TEXT", nullable: false),
+                    IdProperty = table.Column<string>(type: "TEXT", nullable: true),
+                    File = table.Column<string>(type: "TEXT", nullable: true),
+                    Enable = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyImage", x => x.IdPropertyImage);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyTrace",
+                columns: table => new
+                {
+                    IdPropertyTrace = table.Column<string>(type: "TEXT", nullable: false),
+                    DateSale = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Value = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Tax = table.Column<decimal>(type: "TEXT", nullable: false),
+                    IdProperty = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyTrace", x => x.IdPropertyTrace);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Property");
+
+            migrationBuilder.DropTable(
+                name: "PropertyImage");
+
+            migrationBuilder.DropTable(
+                name: "PropertyTrace");
+        }
+    }
+}
